563328271f97e8dd55914935b75b8205afcee008
George G Vega Yon <g.vegayon@gmail.com>
2021-07-08 17:21:50 -0700

    Adding transition prob example

--- parameter-estimates/odds.R
+++ parameter-estimates/odds.R
@@ -1,19 +1,27 @@
-# term_genes_changing(model2fit, duplication = TRUE)
-# term_gains(model2fit, 0:(nfunctions - 1))
-# term_gains(model2fit, 0:(nfunctions - 1), FALSE)
-# term_loss(model2fit, 0:(nfunctions - 1), FALSE)
+library(geese)
 
-coefs <-
-  c(
-    `Overall changes at duplication` = 0,
-    `Overall changes at speciation` = 0,
-    `Num. of genes changing at duplication` = -1.84572360094238,
-    `Gains 0 at duplication` = -1.13923449669951,
-    `Gains 0 at speciation` = -2.49628371040314,
-    `Loss 0 at speciation` = -3.69963383790813,
-    `Root 1` = 5.8333765289455
-  )[-c(1:2, 7)]
+# Building the model
+model2fit <- new_flock()
+add_geese(
+  p           = model2fit,
+  annotations = list(0,0,0),
+  geneid      = c(0,1,2),
+  parent      = c(-1,0,0),
+  duplication = c(TRUE, TRUE, TRUE)
+  )
 
+nfunctions <- 1L
+term_overall_changes(model2fit, duplication = TRUE)
+term_overall_changes(model2fit, duplication = FALSE)
+term_genes_changing(model2fit, duplication = TRUE)
+term_gains(model2fit, 0:(nfunctions - 1))
+term_gains(model2fit, 0:(nfunctions - 1), FALSE)
+term_loss(model2fit, 0:(nfunctions - 1), FALSE)
+
+rule_limit_changes(model2fit, 0, 0, 4, TRUE)
+rule_limit_changes(model2fit, 1, 0, 4, FALSE)
+
+init_model(model2fit)
 
 coefs <- c(
   `Overall changes at duplication` = 0,
@@ -23,7 +31,18 @@ coefs <- c(
   `Gains 0 at speciation` = -2.15269078801566,
   `Loss 0 at speciation` = -3.42075856213656,
   `Root 1` = 1.64267185610882
-)[-c(1:2, 7)]
+)
+
+transition_prob(
+  p           = model2fit,
+  duplication = TRUE,
+  params      = coefs[-7],
+  state       = FALSE,
+  array       = matrix(c(1,1), nrow = 1), as_log = FALSE
+)
+
+coefs <- coefs[-c(1:2, 7)]
+
 
 # Cases: (Spec, Dupl) x (0, 1) x (Gain, Lose) x (Sib 0, Sib 1) -----------------
 
